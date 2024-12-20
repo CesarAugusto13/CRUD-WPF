4bc0052f7fcd4de45b012e5f81728d191d4286ff
Cesar <cesiha13@gmail.com>
2024-12-20 20:25:21 -0300

    Adicionar arquivos de projeto.

---
+++ Cadastro.xaml.cs
@@ -0,0 +1,185 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static TesteJunior.MainWindow;
+
+namespace TesteJunior
+{
+
+    public partial class Cadastro : Window
+    {
+        private string connectionString = "Server=localhost;Database=testdev;Uid=root;Pwd=root;";
+        private Produto _produto;
+
+        public Cadastro(Produto produto = null)
+        {
+            InitializeComponent();
+            CarregarGrupos();
+
+            _produto = produto;
+            if (_produto != null)
+            {
+                txtNomeProduto.Text = _produto.Descricao.ToString();
+                txtPrecoCusto.Text = _produto.PrecoCusto.ToString();
+                txtPrecoVenda.Text = _produto.PrecoVenda.ToString();
+                cmbGrupo.Text = _produto.NomeGrupo.ToString();
+                chkAtivo.IsChecked = _produto.Ativo;
+            }
+        }
+
+        private void CarregarGrupos()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand("SELECT nome FROM produto_grupo", conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cmbGrupo.Items.Add(reader.GetString(0));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar grupos: " + ex.Message);
+            }
+        }
+        private void btnCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            string nomeProduto = txtNomeProduto.Text;
+            decimal precoCusto = Convert.ToDecimal(txtPrecoCusto.Text);
+            decimal precoVenda = Convert.ToDecimal(txtPrecoVenda.Text);
+            string grupoSelecionado = cmbGrupo.SelectedItem.ToString();
+            bool ativo = chkAtivo.IsChecked.Value;
+            DateTime dataHoraCadastro = DateTime.Now;
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(
+                        "INSERT INTO produto (Descricao, PrecoCusto, PrecoVenda, codGrupo, Ativo, DataHoraCadastro) " +
+                        "VALUES (@Descricao, @PrecoCusto, @PrecoVenda, @codGrupo, @Ativo, @DataHoraCadastro)", conn);
+
+                    cmd.Parameters.AddWithValue("@Descricao", nomeProduto);
+                    cmd.Parameters.AddWithValue("@PrecoCusto", precoCusto);
+                    cmd.Parameters.AddWithValue("@PrecoVenda", precoVenda);
+                    switch (grupoSelecionado)
+                    {
+                        case "GERAL":
+                            grupoSelecionado = "1";
+                            break;
+                        case "AÇOUGUE":
+                            grupoSelecionado = "2";
+                            break;
+                        case "HORTIFRUTI":
+                            grupoSelecionado = "3";
+                            break;
+                        case "PADARIA":
+                            grupoSelecionado = "4";
+                            break;
+                        case "GELADEIRA":
+                            grupoSelecionado = "5";
+                            break;
+                    }
+
+                    cmd.Parameters.AddWithValue("@codGrupo", grupoSelecionado);
+                    cmd.Parameters.AddWithValue("@Ativo", ativo);
+                    cmd.Parameters.AddWithValue("@DataHoraCadastro", dataHoraCadastro);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Produto cadastrado com sucesso!");
+                }
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao cadastrar produto: " + ex.Message);
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            string nomeProduto = txtNomeProduto.Text;
+            decimal precoCusto = Convert.ToDecimal(txtPrecoCusto.Text);
+            decimal precoVenda = Convert.ToDecimal(txtPrecoVenda.Text);
+            string grupoSelecionado = cmbGrupo.SelectedItem.ToString();
+            bool ativo = chkAtivo.IsChecked.HasValue && chkAtivo.IsChecked.Value;
+            int cod = _produto.Cod;
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(
+                        "UPDATE produto " +
+                        "SET Descricao = @Descricao, PrecoCusto = @PrecoCusto, PrecoVenda = @PrecoVenda, codGrupo = @codGrupo, Ativo = @Ativo " +
+                        "WHERE cod = @cod", conn);
+
+                    cmd.Parameters.AddWithValue("@Descricao", nomeProduto);
+                    cmd.Parameters.AddWithValue("@PrecoCusto", precoCusto);
+                    cmd.Parameters.AddWithValue("@PrecoVenda", precoVenda);
+
+                    int codGrupo;
+                    switch (grupoSelecionado)
+                    {
+                        case "GERAL":
+                            codGrupo = 1;
+                            break;
+                        case "AÇOUGUE":
+                            codGrupo = 2;
+                            break;
+                        case "HORTIFRUTI":
+                            codGrupo = 3;
+                            break;
+                        case "PADARIA":
+                            codGrupo = 4;
+                            break;
+                        case "GELADEIRA":
+                            codGrupo = 5;
+                            break;
+                        default:
+                            codGrupo = 0;
+                            break;
+                    }
+
+                    cmd.Parameters.AddWithValue("@codGrupo", codGrupo);
+                    cmd.Parameters.AddWithValue("@Ativo", ativo);
+                    cmd.Parameters.AddWithValue("@cod", cod);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Produto editado com sucesso!");
+                }
+
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao editar produto: " + ex.Message);
+            }
+        }
+    } 
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using MySql.Data.MySqlClient;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TesteJunior
+{
+    public partial class MainWindow : Window
+    {
+        private string connectionString = "Server=localhost;Database=testdev;Uid=root;Pwd=root;";
+
+        public class Produto
+        {
+            public int Cod { get; set; }
+            public string Descricao { get; set; }
+            public string NomeGrupo { get; set; }
+            public decimal PrecoCusto { get; set; }
+            public decimal PrecoVenda { get; set; }
+            public bool Ativo { get; set; }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CarregarProdutos();
+
+        }
+
+        public void CarregarProdutos()
+        {
+            try
+            {
+                List<Produto> produtos = new List<Produto>();
+
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(
+                        "SELECT Cod, Descricao, codGrupo, PrecoCusto, PrecoVenda, Ativo " +
+                        "FROM produto", conn);
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Produto produto = new Produto
+                        {
+                            Cod = reader.GetInt32(0),
+                            Descricao = reader.GetString(1),
+                            NomeGrupo = GetNomeGrupo(reader.GetInt32(2)),
+                            PrecoCusto = reader.GetDecimal(3),
+                            PrecoVenda = reader.GetDecimal(4),
+                            Ativo = reader.GetBoolean(5)
+                        };
+                        produtos.Add(produto);
+                    }
+                }
+
+                dgdProdutos.ItemsSource = produtos;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar produtos: " + ex.Message);
+            }
+        }
+        private string GetNomeGrupo(int codGrupo)
+        {
+            switch (codGrupo)
+            {
+                case 1:
+                    return "GERAL";
+                case 2:
+                    return "AÇOUGUE";
+                case 3:
+                    return "HORTIFRUTI";
+                case 4:
+                    return "PADARIA";
+                case 5:
+                    return "GELADEIRA";
+                default:
+                    return "Desconhecido";
+            }
+        }
+        private void RemoverProduto(int Cod)
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    MySqlCommand cmd = new MySqlCommand(
+                        "DELETE FROM produto WHERE Cod = @Cod", conn);
+                    cmd.Parameters.AddWithValue("@Cod", Cod);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao remover produto: " + ex.Message);
+                throw;
+            }
+        }
+
+        private void btnRedirecionarCadastro_Click(object sender, RoutedEventArgs e)
+        {
+            Cadastro Cadastro = new Cadastro();
+            Cadastro.Show();
+            this.Close();
+        }
+
+        private void btnRemover_Click(object sender, RoutedEventArgs e)
+        {
+            Produto produtoSelecionado = dgdProdutos.SelectedItem as Produto;
+            if (produtoSelecionado == null)
+            {
+                MessageBox.Show("Selecione um produto para remover.");
+                return;
+            }
+
+            if (produtoSelecionado.Ativo)
+            {
+                MessageBox.Show("Este produto não pode ser removido porque está ativo.");
+                return;
+            }
+
+            MessageBoxResult result = MessageBox.Show($"Você realmente deseja remover o produto {produtoSelecionado.Descricao}?",
+                                                      "Confirmar Remoção", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    RemoverProduto(produtoSelecionado.Cod);
+                    MessageBox.Show("Produto removido com sucesso!");
+                    CarregarProdutos();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao remover produto: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnRedirecionarEditar_Click(object sender, RoutedEventArgs e)
+        {
+            Produto produtoSelecionado = dgdProdutos.SelectedItem as Produto;
+
+            if (produtoSelecionado != null)
+            {
+                Cadastro cadastro = new Cadastro(produtoSelecionado);
+                cadastro.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Selecione um produto para editar.");
+            }
+        }
+
+
+        private void btnFechar_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
